ab02f9be3d748ab7efeb5a16025ca728f98f92da
LolexDev <150428212+LolexDev@users.noreply.github.com>
2024-02-11 15:40:11 +0100

    Update server.lua

--- l_babice/server.lua
+++ l_babice/server.lua
@@ -1,15 +1,7 @@
-RegisterNetEvent('l_babice:naplati', function (cena)
-    local igrac = ESX.GetPlayerFromId(source)
-    if igrac then
-        igrac.removeMoney(cena)
-    else
-
-    end
-end)
-
 ESX.RegisterServerCallback('l_babice:proveriNovac', function (source,cb,treba)
     local igrac = ESX.GetPlayerFromId(source)
     if igrac.getInventoryItem('money').count >= treba then
+        igrac.removeMoney(treba)
         cb(true)
     else
         cb(false)
